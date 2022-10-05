ac78c02e1193296551804f9188203c31b634d310
alexandravdeenko <alex-avdeenko@yandex.ru>
2022-10-05 15:21:15 +0700

    Сколько чисел больше 0 ввёл пользователь

---
+++ Exam001/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите несколько чисел(через пробел): ");
+string phrase = Console.ReadLine();
+string[] words = phrase.Split(' ');
+int[] numbers = new int[words.Length];
+for (int i = 0; i < words.Length; i++)
+{
+    numbers[i] = int.Parse(words[i]);
+}
+
+int count = 0;
+int size = numbers.Length;
+for (int i = 0; i < size; i++)
+{
+    if (numbers[i] > 0)
+    {
+        count ++;
+    }
+}
+
+Console.WriteLine("Количество чисел больше нуля: " + count);
